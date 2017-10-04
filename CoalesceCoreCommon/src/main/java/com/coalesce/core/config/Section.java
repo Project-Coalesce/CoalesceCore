8547a248b702705fb3f29092918a8cf4ea53735a
NJDaeger <noahdaeger@hotmail.com>
2017-10-04 07:38:31 -0400

    Default methods

--- CoalesceCoreCommon/src/main/java/com/coalesce/core/config/Section.java
+++ CoalesceCoreCommon/src/main/java/com/coalesce/core/config/Section.java
@@ -1,72 +1,33 @@
 package com.coalesce.core.config;
 
-import com.coalesce.core.config.base.BaseConfig;
 import com.coalesce.core.config.base.IConfig;
-import com.coalesce.core.config.base.IEntry;
 import com.coalesce.core.config.base.ISection;
 import com.coalesce.core.plugin.ICoPlugin;
 
-import java.util.Set;
-
 public final class Section implements ISection {
 	
-	private final BaseConfig config;
+	private final ICoPlugin plugin;
+	private final IConfig config;
 	private final String path;
-	private Object value;
 	
-	public Section(String path, Object value, BaseConfig config) {
-		this.path = path;
-		this.value = value;
+	public Section(String path, IConfig config, ICoPlugin plugin) {
+		this.plugin = plugin;
 		this.config = config;
-	}
-	
-	@Override
-	public Set<String> getKeys(boolean deep) {
-		return null;
-	}
-	
-	@Override
-	public Set<IEntry> getEntries() {
-		return null;
-	}
-	
-	@Override
-	public ISection getSection(String path) {
-		return null;
-	}
-	
-	@Override
-	public boolean contains(String path) {
-		return false;
+		this.path = path;
 	}
 	
 	@Override
 	public String getCurrentPath() {
-		return null;
-	}
-	
-	@Override
-	public String getName() {
-		return null;
+		return path;
 	}
 	
 	@Override
 	public IConfig getConfig() {
-		return null;
+		return config;
 	}
 	
 	@Override
 	public ICoPlugin getPlugin() {
-		return null;
-	}
-	
-	@Override
-	public IEntry getEntry(String path) {
-		return null;
-	}
-	
-	@Override
-	public ISection getParent() {
-		return null;
+		return plugin;
 	}
 }
--- CoalesceCoreCommon/src/main/java/com/coalesce/core/config/base/ISection.java
+++ CoalesceCoreCommon/src/main/java/com/coalesce/core/config/base/ISection.java
@@ -1,18 +1,33 @@
 package com.coalesce.core.config.base;
 
+import com.coalesce.core.config.Section;
 import com.coalesce.core.plugin.ICoPlugin;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public interface ISection {
 	
 	/**
-	 * Gets a set of keys that is in this section.
+	 * Gets a set of keys that are in this section.
 	 * @param deep Whether to get the keys of the keys (and so on) in this section.
 	 *
 	 * @return A Set of Keys.
 	 */
-	Set<String> getKeys(boolean deep);
+	default Set<String> getKeys(boolean deep) {
+		Set<String> keys = new HashSet<>();
+		if (deep) {
+			getConfig().getEntries().stream().filter(e -> e.getPath().startsWith(getCurrentPath() + ".")).forEach(e -> keys.add(e.getPath()));
+		}
+		else getConfig().getEntries().stream().filter(e -> e.getPath().startsWith(getCurrentPath()+".")).forEach(e -> {
+			String key = e.getPath().substring(getCurrentPath().length() + 1);
+			int size = key.indexOf(".");
+			if (size < 0) size = key.length();
+			keys.add(key.substring(0, size));
+		});
+		return keys;
+	}
 	
 	/**
 	 * Gets all the entries that exist in this configuration section.
@@ -20,7 +35,9 @@ public interface ISection {
 	 * @return A set of entries in this section.
 	 *
 	 */
-	Set<IEntry> getEntries();
+	default Set<IEntry> getEntries() {
+		return getConfig().getEntries().stream().filter(e -> e.getPath().startsWith(getCurrentPath() + ".")).collect(Collectors.toSet());
+	}
 	
 	/**
 	 * Gets a section from within this section.
@@ -28,14 +45,18 @@ public interface ISection {
 	 *
 	 * @return A section of the configuration.
 	 */
-	ISection getSection(String path);
+	default ISection getSection(String path) {
+		return new Section(getCurrentPath() + "." + path, getConfig(), getPlugin());
+	}
 	
 	/**
 	 * Check whether a section contains a path.
 	 * @param path The path to look for
 	 * @return True if the path exists, false otherwise.
 	 */
-	boolean contains(String path);
+	default boolean contains(String path) {
+		return getKeys(false).contains(path);
+	}
 	
 	/**
 	 * Gets the current path of the configuration section.
@@ -47,7 +68,9 @@ public interface ISection {
 	 * The name of the section.
 	 * @return The name of the section.
 	 */
-	String getName();
+	default String getName() {
+		return getCurrentPath().substring(getCurrentPath().lastIndexOf("."));
+	}
 	
 	/**
 	 * Gets the base configuration of this section.
@@ -66,12 +89,17 @@ public interface ISection {
 	 * @param path The path to the entry. <p>Note: dont use the string provided originally to get this entry</p> //TODO: elaborate more and create getEntry Implementations in Json and yml config.
 	 * @return
 	 */
-	IEntry getEntry(String path);
+	default IEntry getEntry(String path) {
+		return getConfig().getEntry(getCurrentPath() + "." + path);
+	}
 	
 	/**
 	 * Gets the parent section to the current section.
 	 * @return THe previous section, returns null if no previous section exists.
 	 */
-	ISection getParent();
+	default ISection getParent() {
+		if (!getCurrentPath().contains(".")) return null;
+		else return new Section(getCurrentPath().substring(0, getCurrentPath().lastIndexOf(".")), getConfig(), getPlugin());
+	}
 	
 }
